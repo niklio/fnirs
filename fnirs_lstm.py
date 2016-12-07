bca621668a0e5132e500488f0143603291fc706c
samhincks <shincks@d-p-14-142.eecs.tufts.edu>
2016-12-07 11:52:46 -0500

    neural network

--- fnirs_lstm.py
+++ fnirs_lstm.py
@@ -1,28 +1,34 @@
-import os, time
-import numpy as np
-import pandas as pd
-import tensorflow as tf
+import os, time # operating system dependent functionality
+import numpy as np # package for scientific computing on matrixes
+import pandas as pd # data analysis library
+import tensorflow as tf # Google neural network software
 
-from tensorflow.python.ops import rnn, rnn_cell
+# Recurrent neural network http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+# An RNN has a loop so that information persists 
+from tensorflow.python.ops import rnn, rnn_cell 
 
-log_dir = "./logs"
+log_dir = "./logs" #
 
-train_path = "./data/bestemusic/test/00.csv"
-test_path = "./data/bestemusic/test/00.csv"
+train_path ="IDOXY/ID-OXY-1-train.csv"##
+test_path = "IDOXY/ID-OXY-1eva.csv"##"IDOXY/ID-OXY-1.csv"
 
-class_labels = [b'easy', b'hard']
+class_labels = [b'PASSTHOUGHT',b'ADDITION', b'JUNK', b'REST']
+#class_labels = [b'easy', b'hard']
 
 # Training parameters
-batch_size = 100
+batch_size = 50
 learning_rate = 1e-4
 display_step = 1
 
 # Network parameters
-n_inputs = 16
-n_steps = 100
-n_hidden = 32
-n_layers = 2
+n_inputs = 52
+n_steps = 40
+n_hidden = 32 
+n_layers = 2 # 1 layer cannot figure out nonlinearity 
 n_classes = len(class_labels)
+
+
+
 
 print(("\n" +
 "fNIRS long-short term memory network\n" +
@@ -38,10 +44,16 @@ print(("\n" +
     n_hidden=n_hidden,
     n_layers=n_layers))
 
+
+# Given an array of classes, returns a numerical represenation
 def categorical_labels_to_onehot(label_batch, class_labels=None):
     df = pd.DataFrame(label_batch, columns=['label'])
+    # cast to other
     df[df['label'] != class_labels[1]] = class_labels[0]
+
+    # everything that is not the second label becomes the first 
     print(df.groupby('label').size())
+
     onehot_df = pd.get_dummies(df['label'], columns=class_labels)
 
     for label in class_labels:
@@ -49,31 +61,43 @@ def categorical_labels_to_onehot(label_batch, class_labels=None):
             onehot_df[label] = 0
 
     onehot = onehot_df.as_matrix(columns=class_labels)
+
+
+    #onehot: a numerical representation 
+    # these are the output vectors of the NN 
     return onehot
 
+# For a given dataset, retrieve features and label as tensors
 def read_fnirs(filename_queue):
     line_reader = tf.TextLineReader(skip_header_lines=1)
     _, csv_row = line_reader.read(filename_queue)
     record_defaults = [[0.0]] * n_inputs + [[""]]
     cols = tf.decode_csv(csv_row, record_defaults)
-    
-    features = tf.pack(cols[:-1])
+    features = tf.pack(cols[:-1]) # all but the last index
     label = cols[-1]
+
+
     return features, label
 
+# Read the data, given a path and a batch size
+# Moves up one at a time 
 def input_pipeline(path, batch_size=None, n_steps=None):
     filename_queue = tf.train.string_input_producer(
         tf.train.match_filenames_once(path))
 
     features, target = read_fnirs(filename_queue)
+    # Features and targets are now Tensors https://www.tensorflow.org/versions/r0.12/api_docs/python/framework.html#Tensor
     example_sequence, label_sequence = tf.train.batch([features, target],
         batch_size=n_steps)
 
     example_batch, label_batch = tf.train.batch([example_sequence, label_sequence],
         batch_size=batch_size)
 
-    return example_batch, label_batch
+    # keep reading examples until there is an equal distribtion 
 
+    return example_batch, label_batch
+# 
+# 
 def shuffle_input_pipeline(path, batch_size=None, n_steps=None, min_after_dequeue=1000):
     filename_queue = tf.train.string_input_producer(
         tf.train.match_filenames_once(path),
@@ -88,6 +112,7 @@ def shuffle_input_pipeline(path, batch_size=None, n_steps=None, min_after_dequeu
         batch_size=batch_size,
         min_after_dequeue=min_after_dequeue,
         capacity=capacity)
+
 
     return example_batch, label_batch
 
@@ -116,70 +141,144 @@ def RNN(x, weights, biases, is_train=True):
         state_is_tuple=True)
 
     output, states = rnn.rnn(stacked_lstm, x, dtype=tf.float32)
-    return tf.matmul(output[-1], W) + b
+    return tf.matmul(output[-1], weights) + biases
 
 
-print("[*] Building the network ...")
-x = tf.placeholder(tf.float32, shape=[None, n_steps, n_inputs])
-y_ = tf.placeholder(tf.float32, shape=[None, n_classes])
 
-W = weight_variable([n_hidden, n_classes])
-b = bias_variable([n_classes])
 
-y = RNN(x, W, b)
-
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-x_train, y_train = input_pipeline(train_path,
+# Retrieve data
+x_train, y_train = shuffle_input_pipeline(train_path,
     batch_size=batch_size,
     n_steps=n_steps)
 
-init = tf.initialize_all_variables()
 
-print("[*] Training ...")
-with tf.Session() as sess:
+x_test, y_test = shuffle_input_pipeline(test_path,
+    batch_size=batch_size,
+    n_steps=n_steps)
 
-    train_writer = tf.train.SummaryWriter(
-        os.path.join(log_dir, "train"),
-        sess.graph)
 
-    test_writer = tf.train.SummaryWriter(
-        os.path.join(log_dir, "test"),
-        sess.graph)
 
-    sess.run(init)
+# x_train and y_train are tensors linking up to the actual data
+def run(x_train, y_train, x_test, y_test):
+    # TensorFlow provides a placeholder operation that must be fed with data on execution
+    # It's a template, not holding a specific value but rather a preparation for computation.
+    # The computations are happening outside of Python to be efficent, so most interactions
+    # are via preparations of variables and template
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+    # X is a feature in your model. It's a raw datapoint. There are 16 inputs in our model
+    # corresponding to the various channels; however these values change over time
+    # n_steps is the number of subsequent readings in our model; it's 3D input  
 
-    step = 0
-    start_time = time.time()
-    while not coord.should_stop():
-        examples, labels = sess.run([x_train, y_train])
-        labels = categorical_labels_to_onehot(labels[:,-1], class_labels=class_labels)
-        train_accuracy = accuracy.eval(feed_dict={x: examples, y_: labels})
-        train_loss = cost.eval(feed_dict={x: examples, y_: labels})
+    print(n_steps)
+    # Generally, a placeholder has a data type, a shape, and a name; None is left abstract
+    x = tf.placeholder(tf.float32, shape=[None, n_steps, n_inputs])
+    #   x can hold 
 
-        train_summary = tf.Summary(value=[
-            tf.Summary.Value(tag="Accuracy", simple_value=train_accuracy.item()),
-            tf.Summary.Value(tag="Loss", simple_value=train_loss.item())])
+    #There is y and y_; these are our classifications. There are 2 outputs of the model
+    # y_ holds specifically the data type or template of our predictions
+    y_ = tf.placeholder(tf.float32, shape=[None, n_classes])
 
-        train_writer.add_summary(train_summary, step)
+    # W holds the matrix of what will be learned in the actual system. We call this with
+    # a shape; we want a new dimension for every possible class 
+    # and we want a new dimension for the specified number of hidden layers
+    W = weight_variable([n_hidden, n_classes])
 
-        if not step % display_step:
-            print(("step={step}, time={time:.2f}, " +
-                "acc={acc:.2f}, loss={loss:.2f}").format(
-                    step=step,
-                    time=time.time() - start_time,
-                    acc=train_accuracy,
-                    loss=train_loss))
+    # b is the prior probabilities of our classes
+    b = bias_variable([n_classes])
 
-        optimizer.run(feed_dict={x: examples, y_: labels})
-        step += 1
+    # y is the un-run specification of a recurrent neural network, which is a neural
+    # network with loops in it so that it remembers state. 
+    y = RNN(x, W, b)
 
-    coord.request_stop()
-    coord.join(threads)
+    # cost is a cross entropy reduction algorithm which takes into consideration y and y_
+    # which is predictions vs answers
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+
+    #optimizer is a way for updating variables in order to reduce loss. Why not 
+    # gradient descent? Is this an arbitrary choice? 
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # a matrix of correct responses depending on difference between output and ansswer
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+    # The cross entropy representation of correct responses
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    init = tf.initialize_all_variables()
+
+    print("[*] Training ...")
+    with tf.Session() as sess: # initiate session 
+        train_writer = tf.train.SummaryWriter(
+            os.path.join(log_dir, "train"),
+            sess.graph)
+
+        test_writer = tf.train.SummaryWriter(
+            os.path.join(log_dir, "test"),
+            sess.graph)
+
+        sess.run(init)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        step = 0
+        start_time = time.time()
+
+        while not coord.should_stop():
+            examples, labels = sess.run([x_train, y_train])
+            labels = categorical_labels_to_onehot(labels[:,-1], class_labels=class_labels)
+            train_accuracy = accuracy.eval(feed_dict={x: examples, y_: labels})
+            train_loss = cost.eval(feed_dict={x: examples, y_: labels})
+
+            train_summary = tf.Summary(value=[
+                tf.Summary.Value(tag="Accuracy", simple_value=train_accuracy.item()),
+                tf.Summary.Value(tag="Loss", simple_value=train_loss.item())])
+
+            train_writer.add_summary(train_summary, step)
+
+            if not step % display_step:
+                print(("step={step}, time={time:.2f}, " +
+                    "acc={acc:.2f}, loss={loss:.2f}").format(
+                        step=step,
+                        time=time.time() - start_time,
+                        acc=train_accuracy,
+                        loss=train_loss))
+
+            optimizer.run(feed_dict={x: examples, y_: labels})
+            step += 1
+
+            if train_accuracy > 0.9:
+                break
+
+        
+        # move x_test y _test into place holders, input them into computation graph
+        test_examples, test_labels = sess.run([x_test, y_test])
+
+        # converts labels into number vectors
+        test_labels = categorical_labels_to_onehot(test_labels[:,-1], class_labels=class_labels)
+        
+        # feeds into network and classify, pass in each of 50 samples    
+        test_accuracy = accuracy.eval(feed_dict={x: test_examples, y_: test_labels})
+
+        print(test_accuracy)
+
+
+        coord.request_stop()
+        coord.join(threads)
+
+
+print("[*] Building the network ...")
+
+
+init = tf.global_variables_initializer()
+
+run(x_train, y_train, x_test, y_test)
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+filename_queue = tf.train.string_input_producer(["UAV_processed/0.csv", "UAV_processed/1.csv"])
+reader = tf.TextLineReader()
+key, value = reader.read(filename_queue)
+
+# Default values, in case of empty columns. Also specifies the type of the
+# decoded result.
+record_defaults = [[1], [1], [1], [1], [1]]
+col1, col2, col3, col4, col5 = tf.decode_csv(
+    value, record_defaults=record_defaults)
+features = tf.pack([col1, col2, col3, col4])
+
+with tf.Session() as sess:
+  # Start populating the filename queue.
+  coord = tf.train.Coordinator()
+  threads = tf.train.start_queue_runners(coord=coord)
+
+  for i in range(1200):
+    # Retrieve a single instance:
+    example, label = sess.run([features, col5])
+
+  coord.request_stop()
+  coord.join(threads)
